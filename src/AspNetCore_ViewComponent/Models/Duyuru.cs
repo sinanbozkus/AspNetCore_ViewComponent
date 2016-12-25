4ecd2d39f8ce348111c8b45cc30e105b5c9e0cdf
Sinan BOZKUŞ <sinan.bozkus@gmail.com>
2016-12-25 02:00:39 +0200

    Proje dosyaları eklendi.

---
+++ src/AspNetCore_ViewComponent/Models/Duyuru.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_ViewComponent.Models
+{
+    public class Duyuru
+    {
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
---
+++ src/AspNetCore_ViewComponent/ViewComponents/DuyuruViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore_ViewComponent.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_ViewComponent.ViewComponents
+{
+    [ViewComponent]
+    public class DuyuruViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var duyuruList = new List<Duyuru>
+            {
+                new Duyuru
+                {
+                    Baslik = "Duyuru Başlığı 1",
+                    Aciklama = "Duyuru Açıklaması 1"
+                },
+                new Duyuru
+                {
+                    Baslik = "Duyuru Başlığı 2",
+                    Aciklama = "Duyuru Açıklaması 2"
+                },
+                new Duyuru
+                {
+                    Baslik = "Duyuru Başlığı 3",
+                    Aciklama = "Duyuru Açıklaması 3"
+                }
+            };
+
+            return View(duyuruList);
+        }
+    }
+}
